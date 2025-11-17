9baac968fa5232a6f8f42d92578f7ad4d1837a48
Soubhik Mukhopadhyay <sou7bhik@gmail.com>
2025-11-17 20:06:39 +0530

    add 4

--- 01_python-main/02_datatypes/chapter_1.py
+++ 01_python-main/02_datatypes/chapter_1.py
@@ -4,5 +4,9 @@ print(f"Initial sugar: {sugar_amount}")
 sugar_amount = 12
 print(f"Second Initial sugar: {sugar_amount}")
 
+sugar_amount2 = 14
+
+
 print(f"ID of 2: {id(2)}")
 print(f"ID of 12: {id(12)}")
+print(f"ID of 14: {id(14)}")
