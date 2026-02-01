03a7f3262710c0c6916f6f70079a758ca19fe0b8
Soubhik Mukhopadhyay <sou7bhik@gmail.com>
2026-02-01 11:23:37 +0530

    add23

--- Scikit_ML/Udemy/python-math-for-ml/01. Linear Algebra/Week 1/01. System of Equations/03. Ungraded Labs/quiz.py
+++ Scikit_ML/Udemy/python-math-for-ml/01. Linear Algebra/Week 1/01. System of Equations/03. Ungraded Labs/quiz.py
@@ -1,24 +1,28 @@
 import ipywidgets as widgets
 from ipywidgets import interact, Dropdown
 
+question1 = "Select one of the options given:"
 
-question1 = "\033[1mSelect one of the options given:"
-solution1 = {'A':'\033[91mNot quite. While both functions are used to create empty arrays, np.zeros() is initialized with the value 0.',
-'B':"\033[91mNot quite. np.zeros() is inialized, and it gives an output of 0's.",
-'C':"\033[91mNot quite. Most often, np.empty() is faster since it is not initialized.",
-'D':''' \033[92mTrue! np.empty() creates an array with uninitialized elements from available memory space and may be faster to execute.'''}
-
-
+solution1 = {
+    'A': 'Not quite. While both functions are used to create empty arrays, np.zeros() is initialized with the value 0.',
+    'B': 'Not quite. np.zeros() is initialized, and it gives an output of 0s.',
+    'C': 'Not quite. Most often, np.empty() is faster since it is not initialized.',
+    'D': 'True! np.empty() creates an array with uninitialized elements from available memory space and may be faster to execute.'
+}
 
 def mcq(question, solution):
-    s = ''
-#     print(question)
-    print("\033[1mPlease select the correct option:")
-    answer_w = Dropdown(options = solution.keys(), value=None, layout=widgets.Layout(width='25%'))
+    print(question)
+    print("Please select the correct option:")
 
-    @interact(Answer = answer_w)
-    def print_city(Answer):
-        if(Answer != None):
-            s = solution[Answer]
-#             print("\n")
-            print(s)
+    answer_w = Dropdown(
+        options=[('Select an option', None)] + [(k, k) for k in solution.keys()],
+        value=None,
+        layout=widgets.Layout(width='25%')
+    )
+
+    @interact(Answer=answer_w)
+    def show_answer(Answer):
+        if Answer is not None:
+            print(solution[Answer])
+
+mcq(question1, solution1)
