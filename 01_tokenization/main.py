5e7d12a84da7b4986a7224fa9ce1052dca58b6f5
Soubhik Mukhopadhyay <sou7bhik@gmail.com>
2026-02-08 18:37:26 +0530

    add38

--- 01_tokenization/main.py
+++ 01_tokenization/main.py
@@ -8,5 +8,5 @@ tokens = enc.encode(text)
 # Tokens [25216, 3274, 0, 3673, 1308, 382, 336, 7796, 198092]
 print("Tokens", tokens)
 
-decoded = enc.decode([25216, 3274, 0, 3673, 1308, 382, 336, 7796, 198092])
+decoded = enc.decode([25216, 3273, 0, 3673, 1308, 382, 336, 7796, 198092])
 print("Decoded", decoded)
